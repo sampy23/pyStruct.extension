c32a3a3c7c1707cd044ea305c0343de5eaab52bb
sampy23 <sbz5677@gmail.com>
2020-11-28 12:40:38 +0400

    made output warning about current view

--- pyStructure.tab/Basic Tools.panel/Volume.pushbutton/script.py
+++ pyStructure.tab/Basic Tools.panel/Volume.pushbutton/script.py
@@ -7,8 +7,30 @@ from pyrevit import forms
  
 doc =__revit__.ActiveUIDocument.Document
 
+
+
+def unit_from_type(unitype):
+    if unittype == [DB.DisplayUnitType.DUT_CUBIC_FEET]:
+        return "ft^3" 
+    elif unittype == [DB.DisplayUnitType.DUT_CUBIC_METERS]:
+        return "m^3"
+    elif unittype == [DB.DisplayUnitType.DUT_CUBIC_INCHES]:
+        return "in^3"
+    elif unittype == [DB.DisplayUnitType.DUT_CUBIC_CENTIMETERS]:
+        return "cm^3"
+    elif unittype == [DB.DisplayUnitType.DUT_CUBIC_MILLIMETERS]:
+        return "mm^3"
+    elif unittype == [DB.DisplayUnitType.DUT_LITERS]:
+        return "litres"
+    elif unittype == [DB.DisplayUnitType.DUT_GALLONS_US]:
+        return "US gallons"
+    elif unittype == [DB.DisplayUnitType.DUT_CUBIC_YARDS]:
+        return "yd^3"
+
 # make sure active view is not a sheet
 curview = doc.ActiveView
+unit = []
+unitype = []
 
 if isinstance(curview, DB.ViewSheet):
     forms.alert("You're on a Sheet. Activate a model view please.",
@@ -37,12 +59,37 @@ try:
         
         total_volume = 0.0
         for ele in target_elements:
-            vol_para=ele.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
+            vol_para = ele.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
             if vol_para:
-                total_volume+=vol_para.AsDouble()
+                splitted_value = vol_para.AsValueString().split(" ")
+                volume = float(splitted_value[0])
+                if len(splitted_value) > 1:
+                    unit.append(splitted_value[0]) # for area and volume revit return unit
+                else:
+                    unitype.append(vol_para.DisplayUnitType) # for volume unit is not returned so we use unit type
 
-    total_volume = total_volume*0.3048*0.3048*0.3048
-    forms.alert("Total Volume of {} in active view is {} m3".format(selected_switch,total_volume),
-                exitscript=True)
-except: # exception to deal with user exiting the application
+                total_volume+=volume
+
+    if unit:
+        unit = list(set(unit))
+        if len(unit) > 1:
+            print("Some issue")
+        else:
+            reqd_unit = unit[0]
+    else:
+        unittype = list(set(unitype))
+        if len(unittype) > 1:
+            print("Some issue")
+        else:
+            reqd_unit = unit_from_type(unittype[0])
+
+    if total_volume:
+        # total_volume = total_volume*0.3048*0.3048
+        forms.alert("Total volume of {0} in current view is {1} {2}".format(selected_switch,total_volume,reqd_unit),
+                        exitscript=True)
+    else:
+        forms.alert("Total volume negligible in {0} units\n Change project units for result".format(reqd_unit)
+                                                                                                ,exitscript=True)
+except Exception as e: # exception to deal with user exiting the application
+    print(str(e))
     pass 
