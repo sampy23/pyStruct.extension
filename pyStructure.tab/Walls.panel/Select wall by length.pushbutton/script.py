7b05680be058e93475585439f04955c59cf3b36f
sampy23 <sbz5677@gmail.com>
2020-12-04 15:37:32 +0400

    shift in wall selection addin behaviour

--- pyStructure.tab/Walls.panel/Select wall by length.pushbutton/script.py
+++ pyStructure.tab/Walls.panel/Select wall by length.pushbutton/script.py
@@ -7,41 +7,29 @@ from pyrevit import revit, DB
 from pyrevit import forms
 
 #getting selection from user
-__context__ = 'Selection'
+# __context__ = 'Selection'
 
 doc =__revit__.ActiveUIDocument.Document
 uidoc =__revit__.ActiveUIDocument
+curview = doc.ActiveView # gets current view
 
-# make sure active view is not a sheet
-curview = doc.ActiveView
 
 if isinstance(curview, DB.ViewSheet):
     forms.alert("You're on a Sheet. Activate a model view please.",
                 exitscript=True)
 
-selection = revit.get_selection()
-for ele in selection:
-    category_name = ele.Category.Name
-    category_id = ele.Category.Id
-    type_id = ele.GetTypeId()
-    if category_name == "Walls":
-        cparam = ele.Parameter[DB.BuiltInParameter.CURVE_ELEM_LENGTH]
-        cparam = cparam.AsDouble()*.3048
-        target_parameter =  DB.BuiltInParameter.CURVE_ELEM_LENGTH # FAMILY_TOP_LEVEL_PARAM, FAMILY_BASE_LEVEL_PARAM
-    else:
-        print("Sorry!! This addin is only for Walls")
-        exit()
-    
+length = float(forms.ask_for_string("Enter length in meters"))
+target_parameter =  DB.BuiltInParameter.CURVE_ELEM_LENGTH # FAMILY_TOP_LEVEL_PARAM, FAMILY_BASE_LEVEL_PARAM
 param_id = DB.ElementId(target_parameter)
 param_prov = DB.ParameterValueProvider(param_id)
 param_equality = DB.FilterNumericEquals() # equality class
     
-value_rule = DB.FilterDoubleRule(param_prov,param_equality,cparam/0.3048 ,1e-3) # 0.5 will give us a tolerance of 304.8 mm
+value_rule = DB.FilterDoubleRule(param_prov,param_equality,length/0.3048 ,1e-3) # 0.5 will give us a tolerance of 304.8 mm
 param_filter = DB.ElementParameterFilter(value_rule)
 
 same_cat_elements = \
-        DB.FilteredElementCollector(revit.doc,curview.Id)\
-          .OfCategoryId(category_id)\
+        DB.FilteredElementCollector(doc,curview.Id)\
+          .OfCategory(DB.BuiltInCategory.OST_Walls)\
           .WhereElementIsNotElementType()\
           .WherePasses(param_filter)\
           .ToElements()
@@ -57,6 +45,6 @@ for sim_element in same_cat_elements:
 
 if len(filered_elements) > 1:
     revit.get_selection().set_to(filered_elements) 
-    forms.alert("{} Walls of length {} m selected!!!".format(num_walls,cparam))
+    forms.alert("{} Walls of length {} m selected!!!".format(num_walls,length))
 else:
-    forms.alert("No other walls of length {} m found!!!".format(cparam))
+    forms.alert("No other walls of length {} m found!!!".format(length))
