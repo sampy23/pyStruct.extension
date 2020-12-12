8ff733563337ee9a143e8c0ae0825fbba59b0c0d
sampy23 <sbz5677@gmail.com>
2020-12-12 16:48:46 +0400

    added main body of action to the module

--- pyStructure.tab/Walls.panel/walls.stack/wallmagic.py
+++ pyStructure.tab/Walls.panel/walls.stack/wallmagic.py
@@ -25,7 +25,6 @@ def type_from_unit(unit_type):
     else: # anything other than above is assumed to be in feets.so no need for conversion
         return None
 
-
 def get_type(ele,builtin_enum):
     dut = ele.Parameter[builtin_enum].DisplayUnitType
     return unit_from_type(dut)
@@ -51,3 +50,31 @@ def print_output(num_walls,length,cur_units,equality):
     else:
         forms.alert("No other walls of length {} {} m found!!!".format(equality,length))
 
+def action(doc,curview,filter_class,eq_symbol):
+    walls = \
+            DB.FilteredElementCollector(doc,curview.Id)\
+            .OfCategory(DB.BuiltInCategory.OST_Walls)\
+            .WhereElementIsNotElementType()\
+            .ToElements()
+    if walls:
+        target_parameter =  DB.BuiltInParameter.CURVE_ELEM_LENGTH 
+        cur_units = get_type(walls[0],target_parameter)
+        length = float(forms.ask_for_string("Enter length in {0}".format(cur_units)))
+        length_feet = convert_to_internal(length,cur_units)
+        param_filter = filter_rule(target_parameter,filter_class,length_feet)
+        
+        same_cat_elements = \
+                DB.FilteredElementCollector(doc,curview.Id)\
+                .OfCategory(DB.BuiltInCategory.OST_Walls)\
+                .WhereElementIsNotElementType()\
+                .WherePasses(param_filter)\
+                .ToElements()   
+                
+        filered_elements = [i.Id for i in same_cat_elements]    
+        num_walls = len(filered_elements)
+        revit.get_selection().set_to(filered_elements) 
+
+        print_output(num_walls,length,cur_units,eq_symbol)
+    else:
+        forms.alert("No wall found in current view",
+                    exitscript=True)
