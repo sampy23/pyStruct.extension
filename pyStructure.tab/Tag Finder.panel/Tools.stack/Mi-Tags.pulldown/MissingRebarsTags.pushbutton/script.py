f247462d89aad098d638b04f1734722d0cbdd17c
sampy23 <sbz5677@gmail.com>
2025-07-29 20:04:44 +0400

    made changes for revit 2023

--- pyStructure.tab/Tag Finder.panel/Tools.stack/Mi-Tags.pulldown/MissingRebarsTags.pushbutton/script.py
+++ pyStructure.tab/Tag Finder.panel/Tools.stack/Mi-Tags.pulldown/MissingRebarsTags.pushbutton/script.py
@@ -2,16 +2,15 @@ __doc__="This scripts selects Rebars with no associated tags in current view."
 __title__="Missing Rebar\nTag Finder" #Title of the extension
 __author__ = "Shahabaz Sha"
 
-
 #pylint: disable=import-error,invalid-name
 from collections import namedtuple
-
 from pyrevit import revit, DB
 from pyrevit import forms
+from pyrevit import HOST_APP
 
+from System.Collections.Generic import List
 
 Taggable = namedtuple('Taggable', ['tag_type', 'element_type'])
-
 
 doc =__revit__.ActiveUIDocument.Document
 
@@ -21,7 +20,6 @@ curview = doc.ActiveView
 if isinstance(curview, DB.ViewSheet):
     forms.alert("You're on a Sheet. Activate a model view please.",
                 exitscript=True)
-
 
 target = Taggable(
     tag_type=DB.BuiltInCategory.OST_RebarTags,
@@ -44,8 +42,15 @@ tagged_elements = []
 untagged_elements = []
 for eltid in target_tags:
     elt = revit.doc.GetElement(eltid)
-    if elt.TaggedLocalElementId != DB.ElementId.InvalidElementId:
-        tagged_elements.append(elt.TaggedLocalElementId.IntegerValue)
+    if HOST_APP.is_newer_than(2023):
+        if elt.GetTaggedLocalElementIds() != DB.ElementId.InvalidElementId:
+            tagged_elements.append(List[DB.ElementId](elt.GetTaggedLocalElementIds())[0].Value)
+    elif HOST_APP.is_newer_than(2022, or_equal=True):
+        if elt.GetTaggedLocalElementIds() != DB.ElementId.InvalidElementId:
+            tagged_elements.append(List[DB.ElementId](elt.GetTaggedLocalElementIds())[0].IntegerValue)
+    else:
+        if elt.TaggedLocalElementId != DB.ElementId.InvalidElementId:
+            tagged_elements.append(elt.TaggedLocalElementId.IntegerValue)
 
 for elid in target_elements:
     el = revit.doc.GetElement(elid)
